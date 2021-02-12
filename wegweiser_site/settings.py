f928430b9c58f364cb64fd46edf633e73d614e20
l-sin <sin.larry.pt@gmail.com>
2021-02-12 11:48:51 +0100

    Configure static paths

--- wegweiser_site/settings.py
+++ wegweiser_site/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
@@ -64,11 +63,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
-
-#MIDDLEWARE_CLASSES = (
-#    # Simplified static file serving.
-#    # https://warehouse.python.org/project/whitenoise/
-#    'whitenoise.middleware.WhiteNoiseMiddleware',)
 
 ROOT_URLCONF = 'wegweiser_site.urls'
 
@@ -133,9 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv('STATIC_URL') #'/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = []
