f1946b7ac432c5c351abed95358987cdd8896030
l-sin <sin.larry.pt@gmail.com>
2021-02-12 12:15:34 +0100

    Added procfile, installed django-heroku

--- wegweiser_site/settings.py
+++ wegweiser_site/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
@@ -127,7 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = os.getenv('STATIC_URL') #'/static/'
+STATIC_URL = '/static/ '#os.getenv('STATIC_URL') #'/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = []
+django_heroku.settings(locals())
